26b71fa40488190191e3bdf9a9754c6134be56aa
Himanshu Dholakia <hdholakia@samyak.com>
2022-09-20 18:08:58 +0530

    Himanshu - commit the changes

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿// <copyright file="UserController.cs" company="MNX Global Logistics">
+// Copyright (c) MNX Global Logistics. All rights reserved.
+// </copyright>
+// <summary> Operation Related to UserController Class.</summary>
+namespace TCAcknowledge.Api.Controllers
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using DevOps.Dto.Model;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using MNX.ConnectEcho.Common.Models;
+    using MNX.ConnectEcho.Common.Models.Dto;
+    using MNX.ConnectEcho.Common.Models.Enum;
+
+    /// <summary>
+    /// Operation Related to Order UserController.
+    /// </summary>
+    [Route("api/v{version:apiVersion}")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly LoggingInfo applicationLogInfo;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserController"/> class.
+        /// </summary>
+        /// <param name="applicationLogInfo">instance of applicationLogInfo.</param>
+        public UserController(LoggingInfo applicationLogInfo)
+        {
+            this.applicationLogInfo = applicationLogInfo;
+        }
+
+        /// <summary>
+        /// Ordered Event Processor Operation.
+        /// </summary>
+        /// <param name="request">jobGUID and taskGUID request param.</param>
+        /// <returns>ActionResult.</returns>
+        [HttpGet("GetUser")]
+        public async Task<IActionResult> GetUser()
+        {
+            try
+            {
+                applicationLogInfo.APICallStartTime = DateTime.UtcNow;
+                Response<string> responseDto = new Response<string>();
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                responseDto.StatusCode = HttpStatusCode.OK;
+                applicationLogInfo.RequestStatus = RequestStatus.Success.ToString();
+                applicationLogInfo.APICallEndTime = DateTime.UtcNow;
+                return Ok(responseDto);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Ordered Event Processor Operation.
+        /// </summary>
+        /// <param name="insertUserDto">insertUserDto request param.</param>
+        /// <returns>ActionResult.</returns>
+        [HttpPost("InsertUser")]
+        public async Task<IActionResult> InsertUser(InsertUserDto insertUserDto)
+        {
+            try
+            {
+                applicationLogInfo.APICallStartTime = DateTime.UtcNow;
+                Response<string> responseDto = new Response<string>();
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                responseDto.StatusCode = HttpStatusCode.OK;
+                applicationLogInfo.RequestStatus = RequestStatus.Success.ToString();
+                applicationLogInfo.APICallEndTime = DateTime.UtcNow;
+                return Ok(responseDto);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
